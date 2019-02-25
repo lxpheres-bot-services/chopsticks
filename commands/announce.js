6fb3cceaee771e15643550839dfcf9293b9e6a16
lxphere <47260747+lxphere@users.noreply.github.com>
2019-02-25 18:25:40 -0500

    Create announce.js

---
+++ commands/announce.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+	if (!message.member.hasPermission("MOVE_MEMBERS")) return message.reply("You cannot run this command. You must be a HR+!");
+	let embed = new Discord.RichEmbed()
+		.setTitle("Chopsticks Announcement")
+		.setColor("#ff4b4b")
+		.setDescription(args.join(" "))
+		.setFooter(`Announcement made by ${message.author.tag}`, message.author.displayAvatarURL)
+ 		.setThumbnail("https://cdn.discordapp.com/attachments/546368270089977856/549733601470840834/Adobe_Post_20190222_155928.png");
+	bot.channels.get("546347946200006687").send({embed: embed}).then(() => {
+		message.reply("Message sent!");
+	}).catch(() => {
+		message.reply("Something went wrong when announcing, please check my permissions and try again.");
+	});
+};
+module.exports.help = {
+	name: "announce",
+	usage: "announce <txt>",
+	description: "Announcement command, no ping.",
+	longdes: "An announcement command, with no ping.",
+	mentionedperm: "MANAGE_ROLES",
+	category: "Utility"
+};
