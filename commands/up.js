d309ea5ece61c4616dc3c6a30ba2430c31f62bc4
lxphere <47260747+lxphere@users.noreply.github.com>
2019-02-26 15:29:18 -0500

    Create up.js

---
+++ commands/up.js
@@ -0,0 +1,42 @@
+module.exports.run = async (bot, message) => {
+	const client = bot;
+
+	let totalSeconds = process.uptime();
+	//let realTotalSecs = Math.floor(totalSeconds % 60);
+	let days = Math.floor((totalSeconds % 31536000) / 86400);
+	let hours = Math.floor((totalSeconds / 3600) % 24);
+	let minutes = Math.floor((totalSeconds / 60) % 60);
+	let seconds = Math.round(totalSeconds % 60);
+
+	let up = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+	message.channel.send({embed: {
+		color: 15158332,
+		author: {
+			name: client.user.username,
+			icon_url: client.user.avatarURL
+		},
+		title: "\n",
+		description: "\n",
+		fields: [{
+			name: "Chopstick Bot Uptime",
+			value: up,
+			inline: true
+		},
+		],
+		timestamp: new Date(),
+		footer: {
+			icon_url: `${message.author.avatarURL}`,
+			text: `Command ran by ${message.author.tag}`
+		}
+	}
+	});
+};
+
+module.exports.help = {
+	name: "up",
+	usage: "up",
+	description: "nil",
+	longdes: "The amount of time the bot has been up.",
+	mentionedperm: "None",
+	category: "Utility"
+};
