90637e673ddb2bd76a0b2b37dadfc2b9334fa7d3
lxphere <47260747+lxphere@users.noreply.github.com>
2019-05-21 18:10:57 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ module.exports.run = async () => {
 		client.user.setPresence({ game: { type: 3, name: "Kauai Hotels! - ;help" }, status: "online" });
 		console.log(`${client.user.tag} has started!`);
 		await noblox.cookieLogin(process.env.key);
-			});
+			
 		//rbx.login({ username: "CreamyzRankingBot", password: process.env.rblx});
 	});
 
