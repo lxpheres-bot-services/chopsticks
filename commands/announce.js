bbc3ce5cc540c2a976ea06f993ea7c0d42e769e6
lxphere <47260747+lxphere@users.noreply.github.com>
2019-05-21 17:55:35 -0400

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -1,14 +1,14 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-	if (!message.member.roles.get("542492271342518272")) return message.reply("Nope, not today! You need to be an HR+, nice try though.").catch(() => bot.safeSend(message, module.exports.help.name));
+	if (!message.member.roles.get("579601910890233856")) return message.reply("Nope, not today! You need to be an SHR+, nice try though.").catch(() => bot.safeSend(message, module.exports.help.name));
 		let embed = new Discord.RichEmbed()
-		.setTitle("Soy House Announcement")
+		.setTitle("K | Kauai Announcement")
 		.setColor("#ff4b4b")
 		.setDescription(args.join(" "))
-		.setFooter(`Announcement made by ${message.author.tag}`, message.author.displayAvatarURL)
- 		.setThumbnail("https://cdn.discordapp.com/attachments/540914985036021770/552983211794235394/Adobe_Post_20190306_081657.jpg");
-	bot.channels.get("540914985036021770").send({embed: embed}).then(() => {
+		.setFooter(`This announcement was made by ${message.author.tag}`, message.author.displayAvatarURL)
+ 		.setThumbnail("https://cdn.discordapp.com/attachments/438816225909276683/580512978277761025/image0.png");
+	bot.channels.get("579405809319018497").send({embed: embed}).then(() => {
 		message.reply("Message sent!");
 	}).catch(() => {
 		message.reply("Something went wrong when announcing, please check my permissions and try again.");
